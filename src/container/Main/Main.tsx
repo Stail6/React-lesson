8c8b5a94cf4964799af5d3583ee0b05d84220464
Roman6 <romanmartynyuk6038@gmail.com>
2023-02-02 19:32:46 +0200

    add type

--- src/container/Main/Main.tsx
+++ src/container/Main/Main.tsx
@@ -1,5 +1,16 @@
+import { Container } from '@mui/material'
+import Home from 'Pages/Home/Home'
+
 type Props = {}
 const Main = (props: Props) => {
-    return <div>Main</div>
+    return (
+        <Container
+            sx={{
+                padding: '60px 0',
+            }}
+        >
+            <Home />
+        </Container>
+    )
 }
 export default Main
